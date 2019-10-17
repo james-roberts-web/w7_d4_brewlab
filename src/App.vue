f374fdc61f363c04dd593c8255124ac9db765a97
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-17 13:20:30 +0100

    api commit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,30 @@
 </template>
 
 <script>
+import { eventBus } from './main.js';
+
 export default {
+  name: 'app',
+  data () {
+    return {
+      beers: []
+    };
+  },
+
+  mounted () {
+    this.getBeers()
+  },
+
+  methods : {
+    getBeers: function () {
+      fetch('https://api.punkapi.com/v2/beers')
+      .then (res => res.json())
+      .then (beers => this.beers = beers)
+    }
+  }
 }
 </script>
+
 
 <style lang="css" scoped>
 </style>
