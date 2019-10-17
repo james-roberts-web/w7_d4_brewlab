1a8f033637d74e988e70396e9b9c2e67a806289e
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-17 14:22:45 +0100

    listing commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template lang="html">
-  <h1>BOOOOOODOG LAB</h1>
+  <div>
+    <h1>BOOOOOODOG LAB</h1>
+    <beers-list :beers='beers'></beers-list>
+  </div>
 </template>
 
 <script>
+
+import BeersList from './components/BeersList.vue';
+import ListItem from './components/ListItem.vue';
 import { eventBus } from './main.js';
 
 export default {
@@ -14,17 +20,17 @@ export default {
   },
 
   mounted () {
-    this.getBeers()
-  },
-
-  methods : {
-    getBeers: function () {
       fetch('https://api.punkapi.com/v2/beers')
       .then (res => res.json())
       .then (beers => this.beers = beers)
-    }
+  },
+
+  components: {
+    "beers-list": BeersList
   }
 }
+
+
 </script>
 
 
