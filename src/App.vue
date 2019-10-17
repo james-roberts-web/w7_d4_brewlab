ecc15cec9984ab3d09538c4125a44973b7299c92
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-17 15:26:03 +0100

    beer detail showing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template lang="html">
   <div>
     <h1>BOOOOOODOG LAB</h1>
-    <beers-list :beers='beers'></beers-list>
+      <div class="main-container">
+        <beers-list :beers='beers'></beers-list>
+        <beer-detail :beer='selectedBeer'></beer-detail>
+      </div>
   </div>
 </template>
 
@@ -9,13 +12,16 @@
 
 import BeersList from './components/BeersList.vue';
 import ListItem from './components/ListItem.vue';
+import BeerDetail from './components/BeerDetail.vue';
+
 import { eventBus } from './main.js';
 
 export default {
   name: 'app',
   data () {
     return {
-      beers: []
+      beers: [],
+      selectedBeer: null
     };
   },
 
@@ -23,10 +29,15 @@ export default {
       fetch('https://api.punkapi.com/v2/beers')
       .then (res => res.json())
       .then (beers => this.beers = beers)
+
+      eventBus.$on('beer-selected', (beer) => {
+        this.selectedBeer = beer
+      })
   },
 
   components: {
-    "beers-list": BeersList
+    "beers-list": BeersList,
+    "beer-detail": BeerDetail,
   }
 }
 
@@ -35,4 +46,9 @@ export default {
 
 
 <style lang="css" scoped>
+.main-container {
+width: 1000px;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div v-if='beer'>
+    <h2>{{ beer.name }}</h2>
+    <h3>ABV: {{ beer.abv }}</h3>
+  </div>
+
+
+  <!-- <p>BEERSH BEERSH BEERSH BEERSH BEERSH</p> -->
+</template>
+
+<script>
+import { eventBus } from '../main.js';
+export default {
+  name: 'beer-detail',
+  props: ['beer']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
